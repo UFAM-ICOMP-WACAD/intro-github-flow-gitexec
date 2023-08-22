02bc07d0e5775954c588e8ed1dfd36436908921c
cdaniel <cdaniel@ufam.edu.br>
2023-08-21 23:34:32 -0400

    Feat: Este commit, se aplicado adiciona a funções de validar_email() em Phyton.

--- src/validador.py
+++ src/validador.py
@@ -1,7 +1,13 @@
 # Conjunto de REGEX para validação de entradas.
+import re
+
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 
 def validar_email(email):
-  # escreva seu código aqui
+    if re.fullmatch(regex, email):
+      print("Valid email")
+    else:
+      print("Invalid email")
   
 def validar_cep(cep):
   # escreva seu código aqui
@@ -11,6 +17,3 @@ def validar_cpf(cpf):
   
 def validar_telefone(telefone):
   # escreve seu código aqui
-  
- 
-  
